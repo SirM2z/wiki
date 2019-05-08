715137f267afa806e58b573ea5923b9f157da993
sirm2z <sirm2zisv@gmail.com>
2019-05-08 14:34:54 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# 个人 wiki
+
+存放以下且不限于：
+
+- 常用 `code snippets`
+- 踩坑记录
+
+注：多为短小笔记（提醒自己：长篇大论需整理成 blog ） :stuck_out_tongue_closed_eyes:
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: "Ryan's wiki",
+  description: 'Just playing around',
+  base: '/wiki/', // 引用 public 中静态资源： $withBase('/foo.png')
+  markdown: {
+    lineNumbers: true
+  }
+}
